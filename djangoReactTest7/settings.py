c7812bbfbef124290dea03b0954ccee9c0bc54ba
A.M. <andrej2000aa@gmail.com>
2023-11-17 11:04:08 +0100

    jiji

--- djangoReactTest7/settings.py
+++ djangoReactTest7/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'djangoReactTest7.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'build')]
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/build')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -126,7 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(
+    BASE_DIR, 'staticfiles', 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(
+    BASE_DIR, 'frontend', 'build')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -144,4 +150,4 @@ REST_FRAMEWORK = {
      )
  }
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
